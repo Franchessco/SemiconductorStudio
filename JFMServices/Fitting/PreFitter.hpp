55f5525f2a47e6aac831115742d39ead9c8d4d0d
Sir James Clark Maxwell <71722499+SirJamesClarkMaxwell@users.noreply.github.com>
2024-09-02 19:18:56 +0200

    resolving problems

--- JFMServices/Fitting/PreFitter.hpp
+++ JFMServices/Fitting/PreFitter.hpp
@@ -1,44 +1,48 @@
 #pragma once
 #include "pch.hpp"
 #include "JFMIFitting.hpp"
+
 namespace JFMService
 {
-    class AbstractPreFit
-    {
-    public:
-        std::pair<size_t, size_t> RangeData(const FittingService::PlotData &characteristic);
-        virtual FittingService::ParameterMap Estimate(const FittingService::EstimateInput &input) = 0;
+	class AbstractPreFit
+	{
+	public:
+		std::pair<size_t, size_t> RangeData(const FittingService::PlotData& characteristic);
+		virtual FittingService::ParameterMap Estimate(const FittingService::EstimateInput& input) = 0;
 
-    private:
-        size_t getLowerRange(const FittingService::PlotData &characteristic);
-        size_t getUpperRange(const FittingService::PlotData &characteristic);
-    };
+	private:
+		size_t getLowerRange(const FittingService::PlotData& characteristic);
+		size_t getUpperRange(const FittingService::PlotData& characteristic);
+	};
 
-    class FourParameterModelPreFit : AbstractPreFit
-    {
-    public:
-        FourParameterModelPreFit();
-        virtual FittingService::ParameterMap Estimate(const FittingService::EstimateInput &input) override;
-        std::pair<size_t, size_t> rangeData(const FittingService::PlotData &characteristic) { return this->RangeData(characteristic); };
+	class FourParameterModelPreFit :public AbstractPreFit
+	{
+	public:
+		FourParameterModelPreFit();
+		FourParameterModelPreFit(const FourParameterModelPreFit&) = default;
+		FourParameterModelPreFit( FourParameterModelPreFit&&) = default;
+		FourParameterModelPreFit& operator=(const FourParameterModelPreFit&) = default;
+		FourParameterModelPreFit& operator=(FourParameterModelPreFit&&) = default;
+		virtual FittingService::ParameterMap Estimate(const FittingService::EstimateInput& input) override;
+		std::pair<size_t, size_t> rangeData(const FittingService::PlotData& characteristic) { return this->RangeData(characteristic); };
 
-    private:
-        std::vector<std::pair<double, double>> m_AMultiplier;
+	private:
+		std::vector<std::pair<double, double>> m_AMultiplier;
 
-    private:
-        double adjustCoefficient(double dV);
-    };
+	private:
+		double adjustCoefficient(double dV);
+	};
 
-    class PreFitter
-    {
-        using PreFitterMap = std::unordered_map<std::string, std::shared_ptr<AbstractPreFit>>;
+	class PreFitter
+	{
+		using PreFitterMap = std::unordered_map<FittingService::ModelID, std::shared_ptr<AbstractPreFit>>;
 
-    public:
-        PreFitter()
-            std::pair<size_t, size_t> RangeData(const FittingService::PlotData &characteristic);
-        FittingService::ParameterMap Estimate(const FittingService::EstimateInput &input);
+	public:
+		PreFitter();
+		std::pair<size_t, size_t> RangeData(const FittingService::PlotData& characteristic);
+		FittingService::ParameterMap Estimate(const FittingService::EstimateInput& input);
 
-    private:
-    private:
-        PreFitterMap preFitterMap{};
-    };
+	private:
+		PreFitterMap preFitterMap{};
+	};
 }
